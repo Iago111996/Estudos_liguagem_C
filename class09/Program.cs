c802e8bd2e9afa0cf13a5163d10d7ba2d3357b61
Iago111996 <iago111996@gmail.com>
2022-02-15 10:41:09 -0300

    add matriz

--- class09/Program.cs
+++ class09/Program.cs
@@ -20,7 +20,29 @@ namespace class09
       vetor[3] = 444;
       vetor[4] = 555;
 
-      System.Console.WriteLine(vetor2[1]);
+      //matriz
+      int[,] matriz = new int[2,3];
+
+      //uma matriz
+      /*
+        line1 => 10 20 30 
+        line2 => 10 20 30 
+      */
+
+      matriz[0,0] = 10;
+      matriz[0,1] = 10;
+      matriz[0,2] = 10;
+
+      matriz[1,0] = 10;
+      matriz[1,1] = 20;
+      matriz[1,2] = 30;
+
+      matriz[2,0] = 10;
+      matriz[2,1] = 20;
+      matriz[2,2] = 30;
+
+      Console.WriteLine(vetor2[1]);
+      Console.WriteLine(matriz[1,2]);
     }
   }
 }
