6d7db5f708d2575d58a3892ee3f4272089883ee4
Iago111996 <iago111996@gmail.com>
2022-02-16 08:20:51 -0300

    add for/while/do-while

--- class10/Program.cs
+++ class10/Program.cs
@@ -7,7 +7,28 @@ namespace Class10
   {
     static void Main(string[] args)
     {
-      
+      int[] num = new int[10];
+
+      int ii = 0;
+
+      do
+      {
+        System.Console.WriteLine("whilw{0}", ii);
+        ii++;
+      } while (ii < 10);
+
+      while (ii < 10)
+      {
+        System.Console.WriteLine("whilw{0}", ii);
+        ii++;
+      }
+
+      for (int i = 0; i < num.Length; i++)
+      {
+        num[i] = 0;
+
+        System.Console.WriteLine("valor{0}: {1}", i, num[i]);
+      }
     }
   }
 
