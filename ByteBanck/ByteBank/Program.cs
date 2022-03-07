de63fbf973aeb050e2763d5d370fe0f6f3467452
Iago111996 <iago111996@gmail.com>
2022-03-07 08:38:10 -0300

    heranca e interface

---
+++ ByteBanck/ByteBank/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ByteBank.Funcionarios;
+
+namespace ByteBank
+{
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+      var carlos = new Funcionario();
+
+      carlos.Nome = "Carlos";
+      carlos.CPF = "000-039-987-23";
+      carlos.Salario = 2000;
+
+      Console.WriteLine();
+      
+    }
+  }
+}
