8dfc192757d7f8ce5aca443e0ef5a010bdf4a7a3
Iago111996 <iago111996@gmail.com>
2022-03-08 08:12:17 -0300

    add new class in /byte-bank

---
+++ ByteBanck/ByteBank/GerenciadorBonificacao.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ByteBank.Funcionarios;
+
+namespace ByteBank
+{
+  public class GerenciadorBonificacao
+  {
+    private double _totalBonificacao;
+    public void Regitrar(Funcionario funcionario)
+    {
+      _totalBonificacao += funcionario.GetBonificacao();
+    }
+    public void Regitrar(Diretor diretor)
+    {
+      _totalBonificacao += diretor.GetBonificacao();
+    }
+
+    public double GetTotalBonificacao()
+    {
+      return _totalBonificacao;
+    }
+  }
+}
--- ByteBanck/ByteBank/Program.cs
+++ ByteBanck/ByteBank/Program.cs
@@ -10,14 +10,26 @@ namespace ByteBank
   {
     static void Main(string[] args)
     {
+      var gereciador = new GerenciadorBonificacao();
+
       var carlos = new Funcionario();
 
       carlos.Nome = "Carlos";
       carlos.CPF = "000-039-987-23";
       carlos.Salario = 2000;
 
+      gereciador.Regitrar(carlos);
+
+      var roberta = new Diretor();
+
+      roberta.Nome = "roberta";
+      roberta.CPF = "000-039-987-23";
+      roberta.Salario = 5000;
+
+      gereciador.Regitrar(roberta);
+
+
       Console.WriteLine();
-      
     }
   }
 }
